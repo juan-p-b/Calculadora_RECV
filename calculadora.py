b3805084740ec8cea2aae77e99567cb9b29a843b
Juan Sebastián Rodríguez Páez <64869092+JuansFQ2000@users.noreply.github.com>
2021-08-23 18:29:18 -0500

    Juan Sebastián Rodríguez Páez : Factorial (#4)
    Factorial por Rodríguez Páez.

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+
+def factorial(a):
+    value = 1
+    for i in range(1,a+1):
+        value = value * i
+    return("El factorial es:",value)
+
 def divide(a,b):
     if (b==0):
         return "El denominador no puede ser igual a 0"
