9d84493b8a8f6ef560988e57618193cbdfcbeded
AnaMontana10 <42219647+AnaMontana10@users.noreply.github.com>
2021-08-23 18:42:28 -0500

    Serie de Fibonacci por Montaña Monoga.
    Serie de Fibonacci por Montaña Monoga.

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,38 @@
 import numpy as np
 
+
+
+def fibonacci(a):
+
+	serie =  0
+	s = 0
+	f0 ,f1  = 0,1  #primeros dos terminos
+	lista  =  []
+
+	#verificar que sea mayor  a  0
+	if a == 0:
+
+		print("El  número debe ser mayor a 0")
+
+	#verificar que no sea un numero decinal
+	elif a / a != 1:
+		print("utilice numero entero")
+
+	elif a == 1:
+		print(f0)
+
+	else:
+		while serie < a:  
+			lista.append(f0)
+			s=  f0  + f1
+			f0  = f1
+			f1 = s
+			serie =  serie + 1 
+			
+	return lista 
+
+
+
 def raiz(a):
   if a<0:
     print('El número no puede ser negativo')
